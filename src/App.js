838c6995eb865cd84310349eebccfa3feb5e9407
Corey Durthaler <corey.durthaler@martinandassoc.com>
2019-07-02 16:31:28 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            isScanning:true,
+            lastScan:"",
+            scans:[]
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown",this._handleKeyDown.bind(this))
+    }
+    componentWillUnmount() {
+        document.removeEventListener("keydown",this._handleKeyDown.bind(this))
+    }
+
+    _handleKeyDown = (e) => {
+        console.log(e);
+        if(e.key === "Enter") {
+            var tmpScans = this.state.scans;
+            tmpScans.push({text:this.state.lastScan})
+            this.setState({ ...this.state, lastScan:"",scans:tmpScans });
+        } 
+        else if(e.key === "Unidentified"){
+            alert("Unidentified key - ",e.code)
+        }
+        else {
+            this.setState({ ...this.state, lastScan: this.state.lastScan+e.key  });
+        }
+        
+    }
+
+    _handleOnInput = (e) => {
+        console.log("OnInput")
+        console.log(e)
+    }
+
+    _handleOnSelect = (e) =>{
+        alert("on select")
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <h1>Scanner Demo</h1>
         </header>
+        <ScannerInput isScanning={this.state.isScanning} scan = {this.state.lastScan} InputEvent={this.state._handleOnInput} SelectEvent={this.state._handleOnSelect}/>
+        <Results scans={this.state.scans}/>
       </div>
     );
   }
 }
 
+const ScannerInput = (props) => {
+    return (  
+        <div>
+            <input type="text" placeholder="Scan barcode" readOnly={props.isScanning} value={props.scan}/>
+        </div>
+    );
+}
+const Results  = (props) => {
+    return ( 
+        <div>
+            <ol>
+                {props.scans.map(line  =>{
+                    return <li>{line.text}</li>
+                })}
+            </ol>
+        </div>
+     );
+}
+ 
 export default App;
